f9e1680352037fb6255685f4d6c9926a811542dc
JSyndicate <jevwilliamson@gmail.com>
2023-04-09 14:06:49 -0500

    Createed Vuejs

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,14 @@
 # Add any form classes for Flask-WTF here
+
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms import TextAreaField
+from wtforms import FileField
+from wtforms.validators import InputRequired
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+
+class MovieForm(FlaskForm):
+    title = StringField('Title', validators=[InputRequired()])
+    description = TextAreaField('Description', validators=[InputRequired])
+    poster = FileField('Movie Poster', validators=[FileRequired(), FileAllowed(['jpg', 'png'])])
+
